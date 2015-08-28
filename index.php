de07f2b3e3837c9338494b298e4b029bd7464142
johannsig <johannsig@gmail.com>
2015-08-28 15:34:13 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sentia sálfræðistofa</title>
+        <link href="content/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="content/css/project.css" rel="stylesheet" type="text/css"/>
+        <script src="content/js/jquery-2.1.1.min.js" type="text/javascript"></script>
+        <script src="content/js/bootstrap.js" type="text/javascript"></script>
+        <script src="content/js/frozenforge.js" type="text/javascript"></script>
+        <script src="content/js/project.js" type="text/javascript"></script>
+    </head>
+    <body style="">
+
+        <div class="container" style="background-color: white;">
+
+            <div class="row" id="header-row">
+                <div class="col-xs-2" style="background-color: dodgerblue;">Blue part</div>
+                <div class="col-xs-8">
+                    <h1>Sentia Sálfræðistofa</h1>
+                </div>
+                <div class="col-xs-2"></div>
+            </div>
+
+            <div class="row">                
+                <div class="col-xs-2">
+                    <div class="row"> 
+                        <div class="col-xs-12" style="min-height: 200px;">White part</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12" style="background-color: khaki; min-height: 198px;">Yellow part</div>
+                    </div>
+                </div>
+
+                <div class="col-xs-8">
+
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <ul class="nav nav-pills" role="tablist" style="margin: 10px 0px;">
+                                <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Sentia</a></li>
+                                <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Starfsfólk</a></li>
+                                <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Námskeið</a></li>
+                                <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Fræðsluefni</a></li>
+                            </ul>
+                        </div>
+                    </div>                    
+
+                    <div class="row">
+                        <div class="col-xs-12" style="min-height: 338px;">
+                            <div class="tab-content">
+                                <div role="tabpanel" class="tab-pane active" id="home">
+                                    <h1>Soffía Elín Sigurðardóttir</h1>
+
+                                    <h2>Menntun:</h2>
+                                    <p>Soffía Elín er með mastersgráðu í skóla- og þroskasálfræði frá Western Sydney University (WSU) Ástralíu árið 2008 en lauk grunnnámi í sálfræði frá Háskóla Íslands árið 2003. Í dag leggur Soffía Elín stund á doktorsnám í Lýðheilsuvísindum við Háskóla Íslands og rannsakar fósturbörn á Íslandi.</p>
+
+                                    <h2>Starfsreynsla:</h2>
+                                    <p>Soffía Elín hefur verið sjálfstætt starfandi sálfræðingur frá árinu 2011 og rekur Sentia Sálfræðistofu.</p>
+                                    <p>Í Sydney, Ástralíu starfaði Soffía Elín í tveimur grunnskólum með börnum á aldrinum 6 til 18 ára þar sem samfélagsaðstæður voru krefjandi og vandkvæði oft mjög flókin. Sálfræðiþjónustan snéri að margþættri samtalsmeðferð og greiningu á frávikum í þroska barna og unglinga undir handleiðslu Tim Hannan klín. taugasálfr. og Dr. Patsy Tremayne íþróttasálfr. og prófessor.</p>
+
+                                    <h2>Sjálfstætt starfandi sálfræðingur</h2>
+
+                                    <p>Soffía Elín hefur verið sjálfstætt starfandi sálfræðingur frá árinu 2011 og sérhæft sig í sálfræðiþjónustu fyrir börn, unglinga og ungmenni. Helstu verkefni snúa að greiningu og meðferð þroskafrávika, hegðunar- og tilfinningavanda. Sálfræðimeðferð snýr að líðan og hegðun barna þar sem unnið er meðal annars með sjálfstyrkingu, félagsfærni og ákveðniþjálfun.</p>
+
+                                    <h2>Sálfræðingur Barnaverndar Reykjavíkur</h2>
+
+                                    <p>Soffía Elín starfaði sem sálfræðingur Barnaverndar Reykjavíkur og kom að fjölbreyttum verkefnum, þá einna helst meðferðar- og greiningarvinnu barna, handleiðslu við kyn- og fósturforeldra. Soffía Elín hélt reglulega klíníska fundi fyrir starfsfólk og sat í þverfaglegum samráðsteymum sem tengiliður við aðrar stofnanir.</p>
+                                    Sálfræðingur á Þjónustumiðstöð Breiðholts</p>
+
+                                    <p>Soffía Elín starfaði um árabil sem skólasálfræðingur á Þjónustumiðstöð Breiðholts í þverfaglegu teymi við greiningar, ráðgjöf og skammtímameðferð barna á leikskóla- og grunnskólastigi.</p>
+                                </div>
+
+                                    <div role="tabpanel" class="tab-pane" id="profile">Profile...</div>
+
+
+
+                                    <div role="tabpanel" class="tab-pane" id="messages">Messages...</div>
+
+
+
+                                    <div role="tabpanel" class="tab-pane" id="settings">Settings...</div>
+                                </div>                            
+                            </div>
+                        </div>
+
+                    </div>                
+
+                    <div class="col-xs-2" style="background-color: brown; min-height: 400px; color: white">
+                        Red part
+                    </div>
+                </div>
+
+
+            </div>
+
+            <?php
+            // put your code here
+            ?>
+    </body>
+</html>
