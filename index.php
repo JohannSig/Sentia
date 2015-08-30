df1ba9422e920ea69568574d76b19bc47d1e653f
johannsig <johannsig@gmail.com>
2015-08-30 10:54:51 +0000

    Content commit

--- index.php
+++ index.php
@@ -23,6 +23,8 @@ and open the template in the editor.
                 <div class="col-xs-2" style="background-color: dodgerblue;">Blue part</div>
                 <div class="col-xs-8">
                     <h1>Sentia Sálfræðistofa</h1>
+                    <p>Höfðabakka 9 Reykjavík</p>
+                    <p>Tímapöntun 527-7600</p>
                 </div>
                 <div class="col-xs-2"></div>
             </div>
@@ -42,7 +44,8 @@ and open the template in the editor.
                     <div class="row">
                         <div class="col-xs-12">
                             <ul class="nav nav-pills" role="tablist" style="margin: 10px 0px;">
-                                <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Sentia</a></li>
+                                <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Sentia</a></li>
+                                <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Þjónusta</a></li>
                                 <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Starfsfólk</a></li>
                                 <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Námskeið</a></li>
                                 <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Fræðsluefni</a></li>
@@ -54,48 +57,96 @@ and open the template in the editor.
                         <div class="col-xs-12" style="min-height: 338px;">
                             <div class="tab-content">
                                 <div role="tabpanel" class="tab-pane active" id="home">
-                                    <h1>Soffía Elín Sigurðardóttir</h1>
+                                    <h1>Sentia Sálfræðistofa</h1>
+                                    <p>Sálfræðiþjónusta fyrir börn, unglinga, ungmenni og fullorðna.</p>
+                                    <p><i>Orðið Sentia er úr latínu og upprunnið í rómverskri goðafræði. Sentia var verndari þroska og menntun barna.</i></p>
 
-                                    <h2>Menntun:</h2>
-                                    <p>Soffía Elín er með mastersgráðu í skóla- og þroskasálfræði frá Western Sydney University (WSU) Ástralíu árið 2008 en lauk grunnnámi í sálfræði frá Háskóla Íslands árið 2003. Í dag leggur Soffía Elín stund á doktorsnám í Lýðheilsuvísindum við Háskóla Íslands og rannsakar fósturbörn á Íslandi.</p>
-
-                                    <h2>Starfsreynsla:</h2>
-                                    <p>Soffía Elín hefur verið sjálfstætt starfandi sálfræðingur frá árinu 2011 og rekur Sentia Sálfræðistofu.</p>
-                                    <p>Í Sydney, Ástralíu starfaði Soffía Elín í tveimur grunnskólum með börnum á aldrinum 6 til 18 ára þar sem samfélagsaðstæður voru krefjandi og vandkvæði oft mjög flókin. Sálfræðiþjónustan snéri að margþættri samtalsmeðferð og greiningu á frávikum í þroska barna og unglinga undir handleiðslu Tim Hannan klín. taugasálfr. og Dr. Patsy Tremayne íþróttasálfr. og prófessor.</p>
-
-                                    <h2>Sjálfstætt starfandi sálfræðingur</h2>
-
-                                    <p>Soffía Elín hefur verið sjálfstætt starfandi sálfræðingur frá árinu 2011 og sérhæft sig í sálfræðiþjónustu fyrir börn, unglinga og ungmenni. Helstu verkefni snúa að greiningu og meðferð þroskafrávika, hegðunar- og tilfinningavanda. Sálfræðimeðferð snýr að líðan og hegðun barna þar sem unnið er meðal annars með sjálfstyrkingu, félagsfærni og ákveðniþjálfun.</p>
-
-                                    <h2>Sálfræðingur Barnaverndar Reykjavíkur</h2>
-
-                                    <p>Soffía Elín starfaði sem sálfræðingur Barnaverndar Reykjavíkur og kom að fjölbreyttum verkefnum, þá einna helst meðferðar- og greiningarvinnu barna, handleiðslu við kyn- og fósturforeldra. Soffía Elín hélt reglulega klíníska fundi fyrir starfsfólk og sat í þverfaglegum samráðsteymum sem tengiliður við aðrar stofnanir.</p>
-                                    Sálfræðingur á Þjónustumiðstöð Breiðholts</p>
-
-                                    <p>Soffía Elín starfaði um árabil sem skólasálfræðingur á Þjónustumiðstöð Breiðholts í þverfaglegu teymi við greiningar, ráðgjöf og skammtímameðferð barna á leikskóla- og grunnskólastigi.</p>
                                 </div>
 
-                                    <div role="tabpanel" class="tab-pane" id="profile">Profile...</div>
+
+                                <div class="row">
+                                    <div class="col-xs-12" style="min-height: 338px;">
+                                        <div class="tab-content">
+                                            <div role="tabpanel" class="tab-pane active" id="home">
+                                                <h1>Meðferð og ráðgjöf Sentia Sálfræðistofa</h1>
+
+                                                <h2>Sálfræðileg meðferð</h2>
+                                                <li>Einstaklingsviðtöl fyrir börn, unglinga og foreldra</li>
+                                                <li>Handleiðsla og ráðgjöf fyrir foreldra</li>
+                                                <li>Fjölskyldumeðferð</li>
+                                                <li>Skype viðtöl</li>
+
+                                                <h2>Sálfræðiathugun</h2>
+                                                <li>Skimun á þroska, hegðun og líðan</li>
+                                                <li>Greining og vitsmunaþroskamat</li>
+                                                <li>Tilvísanir í önnur úrræði eftir sem á við</li>
+                                                <li>Forsjár- og umgengnimál</li>
+
+                                                <h2>Önnur þjónusta</h2>
+                                                <li>Fyrirlestrar</li>
+                                                <li>Námskeið</li>
+                                                <li>Fundir í skóla</li>
+                                                <li>Fundir með skjólstæðingum, foreldrum og öðrum fagaðilum</li>
+                                                <br>
+                                                <br>
+                                            </div>
+
+                                            <div role="tabpanel" class="tab-pane" id="profile">
+
+                                                <h1>Soffía Elín Sigurðardóttir</h1>
+                                                <p>Sálfræðingur</a>
+                                                <p>soffiaelin@sentia.is</p>
+                                                <h2>Menntun:</h2>
+                                                <p>Soffía Elín er með mastersgráðu í skóla- og þroskasálfræði frá Western Sydney University (WSU) Ástralíu árið 2008 en lauk grunnnámi í sálfræði frá Háskóla Íslands árið 2003.</p>
+                                                <p>Í dag leggur Soffía Elín stund á doktorsnám í Lýðheilsuvísindum við Háskóla Íslands og rannsakar fósturbörn á Íslandi. Doktorsrannsóknin er fyrsta sinnar tegundar á Íslandi.</p>
+
+                                                <h2>Starfsreynsla:</h2>
+                                                <p>Starfsreynsla og sérhæfing hefur alfarið snúið að sálfræðiþjónustu fyrir börn, unglinga og ungmenni. Greining og meðferð frávika í þroska, hegðunar- og tilfinningavanda. Soffía Elín vinnur meðal annars með sjálfstyrkingu, félagsfærni og ákveðniþjálfun.</p>
+                                                <p>Soffía Elín hefur verið sjálfstætt starfandi sálfræðingur frá árinu 2011 og rekur Sentia Sálfræðistofu. Samhliða starfar Soffía Elín sem skólasálfræðingur hjá Garðabæ.</p>
+                                                <p>Soffía Elín hefur starfað sem skólasálfræðingur bæði á Íslandi  og í Sydney, Ástralíu þar sem samfélagsaðstæður voru krefjandi og vandkvæði oft mjög flókin. Meðal handleiðara eru þau Tim Hannan klínínskur taugasálfræðingur og Dr. Patsy Tremayne íþróttasálfræðingur og prófessor.</p>
+                                                <p>Soffía Elín starfaði í hátt þrjú ár sem sálfræðingur Barnaverndar Reykjavíkur og kom þar að fjölbreyttum verkefnum sem snéru að meðferðar- og greiningarvinnu barna ásamt handleiðslu við kyn- og fósturforeldra.</p>
+                                                <br>
+                                                <br>
+                                            </div>
 
 
 
-                                    <div role="tabpanel" class="tab-pane" id="messages">Messages...</div>
+                                            <div role="tabpanel" class="tab-pane" id="messages">
+
+                                                <h1>Námskeið og fyrirlestrar</h1>
+
+                                                <h2>Nexus Noobs</h2>
+                                                <p>Námskeiðið er ætlað ungu fólki á aldrinum 12 til 20 ára sem langar til þess að læra inn á ný áhugamál eða að læra meira um viðfangsefnin sem þau þegar þekkja.</p>
+                                                <p>Þátttakendur kynnast frábærum félögum sem deila áhuganum og læra inn á borðspil, safnkortaspil, hlutverkaspil, módelsmíði, herkænskuleiki, vísindaskáldskap, fantasíur, myndasögur og margt fleira.</p>
+                                                <h2>Skráning á haustnámskeið</h2>
+                                                <p>Hægt er að fá frekari upplýsingar um skráningu á námskeiðið á heimasíðu Nexus Noobs eða með tölvupósti.</p>
+                                                <p><a href="https://www.facebook.com/Nexusnoobs">Nexus Noobs á Facebook!</a></p>
+                                                <br>
+                                            </div>
 
 
 
-                                    <div role="tabpanel" class="tab-pane" id="settings">Settings...</div>
-                                </div>                            
+                                            <div role="tabpanel" class="tab-pane" id="settings">
+                                                <p><a href="https://www.nexusnoobs.is//">Nexus Noobs</a></p>
+                                                <p><a href="https://www.khanacademy.org//">Khan Academy</a></p>
+                                                <p><a href="https://www.khanacademy.org//">Khan Academy</a></p>
+                                                <p><a href="https://www.khanacademy.org//">Khan Academy</a></p>
+
+
+
+                                            </div>
+                                        </div>                            
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
-
-                    </div>                
-
-                    <div class="col-xs-2" style="background-color: brown; min-height: 400px; color: white">
-                        Red part
                     </div>
                 </div>
 
-
+                <div class="col-xs-2" style="background-color: brown; min-height: 400px; color: white">
+                    Red part
+                </div>
             </div>
 
             <?php
